099c8e2d7fc6b28f5d49aa8e303a42290e2b7f34
Pedro Moura <pedrorpmoura@gmail.com>
2019-12-09 11:05:11 +0000

    updates

--- fsdtwitter/src/main/java/data/models/Post.java
+++ fsdtwitter/src/main/java/data/models/Post.java
@@ -1,9 +1,16 @@
 package data.models;
 
+import java.time.LocalDate;
+
 public class Post {
 
     private int id;
+    private LocalDate date;
     private String content;
     private User poster;
+
+    public Post(int id, String content, User poster) {
+
+    }
 
 }

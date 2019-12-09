f39b35b2a9e195c54f477e340fa850a41d7ba9de
Pedro Moura <pedrorpmoura@gmail.com>
2019-12-09 10:45:39 +0000

    a

--- README.md
+++
@@ -1,2 +0,0 @@
-# Trabalho Fundamentos de Sistemas Distribuídos
-Trabalho prático da disciplina de Fundamentos de Sistemas Distribuídos (2019/2020)
--- fsdtwitter/src/main/java/net/Message.java
+++
@@ -1,7 +0,0 @@
-package net;
-
-public class Message
-{
-    public int[] vectorClock = new int[Config.MAX_PROCESSES];
-    public byte[] content;
-}

5252e147ddeb875bbcd065fe8d6fcd159048f890
Pedro Moura <pedrorpmoura@gmail.com>
2019-12-20 00:10:58 +0000

    Update Client.java

--- fsdtwitter/src/main/java/client/Client.java
+++ fsdtwitter/src/main/java/client/Client.java
@@ -4,26 +4,26 @@ package client;
 import client.menus.Menu;
 import client.menus.StartMenu;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.channels.AsynchronousSocketChannel;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class Client {
 
     public static void main(String[] args) throws Exception {
 
+        Client client = new Client();
+
         String hostname = "localhost";
 
-        connectToServer(hostname);
+        client.connectToServer(hostname);
 
         Menu menu = new StartMenu();
         menu.run();
     }
 
-    private static void connectToServer(String hostname) throws Exception {
+    private void connectToServer(String hostname) throws Exception {
 
         AsynchronousSocketChannel socketChannel = AsynchronousSocketChannel.open();
         SocketAddress serverAddress = new InetSocketAddress(hostname, 8000);
